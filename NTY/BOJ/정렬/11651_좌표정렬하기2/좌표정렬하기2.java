f99dfc673b96d4e8eb74a2de395a51281e5b23e8
NTY-1017 <nty1017@gmail.com>
2023-01-05 17:26:28 +0900

    BOJ : 11651 정렬 좌표정렬하기2

--- NTY/BOJ/정렬/11651_좌표정렬하기2/좌표정렬하기2.java
+++ NTY/BOJ/정렬/11651_좌표정렬하기2/좌표정렬하기2.java
@@ -17,10 +17,10 @@ public class 좌표정렬하기2 {
         }
 
         Arrays.sort(arr, (o1, o2) -> {
-            if(o1[0] == o2[1]){
+            if(o1[1] == o2[1]){
                 return o1[0] - o2[0];
             } else {
-                return o1[0] - o2[1];
+                return o1[1] - o2[1];
             }
         });
 
