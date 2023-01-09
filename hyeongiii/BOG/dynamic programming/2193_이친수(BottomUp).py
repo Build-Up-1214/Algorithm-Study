30dc57fc17b7e935c01b83b2c4a99784959e2c13
hyeongiii <hyeongiii0811@gmail.com>
2023-01-09 15:45:31 +0900

    BOJ : 2193 이친수(BottomUp) [다이나믹 프로그래밍] 수정

--- hyeongiii/BOG/dynamic programming/2193_이친수(BottomUp).py
+++ hyeongiii/BOG/dynamic programming/2193_이친수(BottomUp).py
@@ -2,15 +2,10 @@
 num = int(input())
 
 # DP 테이블 초기화
-if num == 1:    # num이 1일 경우
-    dp = [0] * 2
-    dp[1] = 1
-else:    # num이 1이 아닐 경우
-    dp = [0] * (num + 1)
-    dp[1] = dp[2] = 1
+dp = [0, 1]
 
 # 다이나믹 프로그래밍 진행 (BottomUp)
-for i in range(3, num + 1):
-    dp[i] = dp[i - 1] + dp[i - 2]
+for i in range(2, num + 1):
+    dp.append(dp[i - 1] + dp[i - 2])
 
 print(dp[num])
