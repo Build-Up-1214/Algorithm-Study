44a62092f093cb80fc09fc42464aa7f0a2c5ee66
NTY-1017 <nty1017@gmail.com>
2023-02-17 23:18:58 +0900

    BOJ : 11403 그래프 경로찾기

---
+++ NTY/BOJ/그래프/11403_경로찾기/경로찾기.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class 경로찾기 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] graph = new int[N][N];
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) graph[i][j] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (graph[i][k] == 1 && graph[k][j] == 1) {
+                        graph[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) sb.append(graph[i][j]).append(' ');
+            sb.append('\n');
+        }
+        System.out.print(sb);
+    }
+}
