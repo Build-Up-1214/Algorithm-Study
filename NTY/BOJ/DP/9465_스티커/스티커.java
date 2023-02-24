6e3b611c438b4b586cfbff5d5b531795a2c0f231
NTY-1017 <nty1017@gmail.com>
2023-02-24 22:58:13 +0900

    BOJ : 9465 DP 스티커

--- NTY/BOJ/DP/9465_스티커/스티커.java
+++ NTY/BOJ/DP/9465_스티커/스티커.java
@@ -17,13 +17,14 @@ public class 스티커 {
             int[][] dp = new int[2][N];
             dp[0][0] = arr[0][0];
             dp[1][0] = arr[1][0];
-            dp[0][1] = dp[1][0] + arr[0][1];
-            dp[1][1] = dp[0][0] + arr[1][1];
-            for (int i = 2; i < N; i++) {
-                dp[0][i] = Math.max(dp[1][i - 1], dp[1][i - 2]) + arr[0][i];
-                dp[1][i] = Math.max(dp[0][i - 1], dp[0][i - 2]) + arr[1][i];
+            if(N != 1) {
+                dp[0][1] = dp[1][0] + arr[0][1];
+                dp[1][1] = dp[0][0] + arr[1][1];
+                for (int i = 2; i < N; i++) {
+                    dp[0][i] = Math.max(dp[1][i - 1], dp[1][i - 2]) + arr[0][i];
+                    dp[1][i] = Math.max(dp[0][i - 1], dp[0][i - 2]) + arr[1][i];
+                }
             }
-
             sb.append(Math.max(dp[0][N - 1], dp[1][N - 1])).append('\n');
         }
         System.out.print(sb);
