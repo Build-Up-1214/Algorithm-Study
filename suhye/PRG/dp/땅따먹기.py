8b762bc82b920f1d5e3f5e8c7f1f634210db7dc9
see1237 <see3722@hanmail.net>
2023-04-01 21:06:59 +0900

    PRG : DP 땅따먹기

---
+++ suhye/PRG/dp/땅따먹기.py
@@ -0,0 +1,11 @@
+def solution(land):
+    for i in range(1, len(land)):
+        #     land[i][0] += max(land[i-1][1], land[i-1][2], land[i-1][3])
+        #     land[i][1] += max(land[i-1][0], land[i-1][2], land[i-1][3])
+        #     land[i][2] += max(land[i-1][0], land[i-1][1], land[i-1][3])
+        #     land[i][3] += max(land[i-1][0], land[i-1][1], land[i-1][2])
+
+        for j in range(len(land[0])):
+            land[i][j] = max(land[i - 1][: j] + land[i - 1][j + 1:]) + land[i][j]
+
+    return max(land[-1])
